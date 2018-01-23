2ee40e455e7310fdbc953ce901341e3ace64ec9c
georn <mrog0@outlook.com>
2018-01-23 20:21:03 +0000

    setting up the ejs as the view engine for the express server

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
 // Set the views folder and the view engine
-app.set('views', './views');
+app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'ejs');
-
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index');
 });
-
 
 // Server listener
 app.listen(PORT, () => {
