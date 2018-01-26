29f5d62e240708ba722cb8c942b12798c53ab87a
georn <mrog0@outlook.com>
2018-01-26 16:21:53 +0000

    Login and persisting session

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+const user = require('./models/User.js');
+const session = require('client-sessions');
+const keys = require('./config/devKeys');
+const bodyParser = require('body-parser');
 const _ = require('lodash');
 
 const app = express();
@@ -8,27 +12,49 @@ const PORT = 3000;
 // Database connection - SPIKE
 require('./services/mongoose');
 
+// bodyParser Middleware Config
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Session Middleware - client-session
+app.use(
+  session({
+    cookieName: 'session',
+    secret: keys.sessionSecret,
+    durantion: 24 * 60 * 60 * 1000 // A day in Miliseconds
+  })
+);
+
 // Set the views folder and the view engine - Middleware
 app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+  console.log(req.session);
   res.render('index');
 });
 
 // Routes - Authentication
 // GET /login
 app.get('/login', (req, res) => {
-  res.send('Login');
+  res.render('login');
 });
 
 // POST /login {username, password} - SPIKED
 app.post('/login', (req, res) => {
-  // console.log('calling');
-  // let dummy = {"hello": "hello"};
-  let body = _.pick(req.body, ['username', 'password']);
+  // let body = _.pick(req.body, ['username', 'password']);
+  // console.log(req.body);
 
-  res.send(body);
+  let user = {
+    username: 'dummyUser',
+    password: 'password'
+  };
+  if (req.body.password === user.password) {
+    req.session.user = user;
+    // res.send(req.session.user);
+    res.redirect('/');
+  } else {
+    console.log('Invalid user credentials');
+  }
 });
 
 // Server listener
